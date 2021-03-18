2ccc304b1fdc288a4f0d3921c7acb971ef0eca55
nadir-ziane <nadir.ziane@laplateforme.io>
2021-03-18 11:54:24 +0100

    jour05

--- jour05/job01/style/script.js
+++ jour05/job01/style/script.js
@@ -1,48 +1,47 @@
 $("#button").click(function() {
-    var nom = document.getElementById("nom").value;
-    var prenom = document.getElementById("prenom").value;
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
+        var nom = document.getElementById("nom").value;
+        var prenom = document.getElementById("prenom").value;
+        var email = document.getElementById("email").value;
+        var password = document.getElementById("password").value;
+        if (nom != "" && prenom != "" && email != "") {
+            if (IsEmail(email) != false) {
+                if (password.length >= 8 && password.match(/[!,@,#,$,%,^,&,*,?,_,~,-,(,),+,=]/) && password.match(/[0-9]/)) {
+                    $.ajax({
+                        url: '../pages/signup.php',
+                        type: "POST",
+                        data: "signUpVerify=1&email=" + email,
+                        success: function(data) {
+                            if (data == "non") {
+                                $.ajax({
+                                    url: '../pages/signup.php',
+                                    type: "POST",
+                                    data: { 'signUpSuccess': 1, 'nom': nom, 'prenom': prenom, 'email': email, 'password': password },
+                                    success: function() {
 
-    if (nom != "" && prenom != "" && email != "") {
-        if (IsEmail(email) != false) {
-            if (password.length >= 8 && password.match(/[!,@,#,$,%,^,&,*,?,_,~,-,(,),+,=]/) && password.match(/[0-9]/)) {
-                $.ajax({
-                    url: '../pages/inscription.php',
-                    type: "POST",
-                    data: "signUpVerify=1&email=" + email,
-                    success: function(data) {
-                        if (data == "non") {
-                            $.ajax({
-                                url: '../pages/inscription.php',
-                                type: "POST",
-                                data: { 'signUpSuccess': 1, 'nom': nom, 'prenom': prenom, 'email': email, 'password': password },
-                                success: function() {
-                                    console.log(password);
-                                    $('#error').css("display", "block");
-                                    document.getElementById('error').innerHTML = "<li>vous etes inscrit</li>";
-                                }
-                            })
-                        } else {
-                            $('#error').css("display", "block");
-                            document.getElementById('error').innerHTML = "<li>Desolé l'Email que vous avez saissir existe deja</li>";
+                                        $('#error').css("display", "block");
+                                        document.getElementById('error').innerHTML = "<li>Vous etes inscrit</li>";
+                                    }
+                                })
+                            } else {
+                                $('#error').css("display", "block");
+                                document.getElementById('error').innerHTML = "<li>Desolé l'Email que vous avez saissir existe deja</li>";
+                            }
                         }
-                    }
-                });
+                    });
+                } else {
+                    $('#error').css("display", "block");
+                    document.getElementById('error').innerHTML = "<li>8 caractere minmum</li><li>un chiffre minimun</li><li>un caractere special (!,@,#,$,%,^,&,*,?,_,~,-,(,),+,=)</li>";
+                }
             } else {
                 $('#error').css("display", "block");
-                document.getElementById('error').innerHTML = "<li>8 caractere minmum</li><li>un chiffre minimun</li><li>un caractere special (!,@,#,$,%,^,&,*,?,_,~,-,(,),+,=)</li>";
+                document.getElementById('error').innerHTML = "<li>Votre email n'est pas un  valide email</li>";
             }
         } else {
             $('#error').css("display", "block");
-            document.getElementById('error').innerHTML = "<li>Votre email n'est pas un  valide email</li>";
+            document.getElementById('error').innerHTML = "<li>Remplissez tous le formulaire svp</li>";
         }
-    } else {
-        $('#error').css("display", "block");
-        document.getElementById('error').innerHTML = "<li>Remplissez tous le formulaire svp</li>";
-    }
-})
-
+    })
+    /// PARTIE CONNEXION ///
 $("#button2").click(function() {
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
@@ -50,9 +49,9 @@ $("#button2").click(function() {
     if (IsEmail(email) != false) {
         if (password != "") {
             $.ajax({
-                url: '../pages/connexion.php',
+                url: '../pages/signin.php',
                 type: "POST",
-                data: { 'connexion': 1, 'email': email, 'password': password },
+                data: { 'signinVerify': 1, 'email': email, 'password': password },
                 success: function(data) {
                     if (data == "oui") {
                         location.replace("../")
